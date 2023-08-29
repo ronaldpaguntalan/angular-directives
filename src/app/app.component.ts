e4a022b78a15bd319e9d3c5eb8b69ee68bd9c612
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-08-29 18:26:18 +0800

    Angular Full Course - Complete Zero to Hero Angular full Tutorial
    Link: https://www.youtube.com/watch?v=CGLdH5ORX-Y
    
    Timestamps:
    02:37:53 Section Intro Directives
    02:38:32 What is Angular Directive?
    02:40:44 NgFor Directive
    02:47:22 Fetch Object Array
    02:53:03 Angular Change Detection
    02:58:18 Usage of Array Index
    03:01:11 NgIf Directive
    03:06:06 NgTemplate Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-directives';
+
+  postArray: Array<string> =  ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5']
+  objArray: Array<any> = [
+    { id: 1, postTitle: 'Post 1',},
+    { id: 2, postTitle: 'Post 2',},
+    { id: 3, postTitle: 'Post 3',},
+    { id: 4, postTitle: 'Post 4',},
+    { id: 5, postTitle: 'Post 5',},
+  ]
+
+  newObjArray: Array<any> = [];
+
+  stepForm: string = "";
+
+
+  constructor(){
+    for(let i = 0; i < this.postArray.length; i++){
+      console.log(this.postArray[i]);
+      
+    }
+  }
+
+  addNew(){
+    this.objArray.push({id: 6, postTitle: 'Post 6'})
+    this.newObjArray.push({id: 7, postTitle: 'Post 7'})
+  }
+
+
+  // Traditional Approach in Java Script
+  /* deleteThis(post: string){
+  //   let index = this.objArray.indexOf(post); // getting the index number we are removing
+
+  //   this.objArray.splice(index, 1); // splice method was utilized to removed the current index, and how many we are deleting
+  } */
+
+
+  deleteThis(index: number){
+    this.objArray.splice(index, 1);
+    this.newObjArray.splice(index, 1);
+  }
 }
